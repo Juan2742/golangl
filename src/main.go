2a3b05a282ca6b189e4d7e290de46065ca0e95a6
Juan2742 <fonsecajuan239@gmail.com>
2025-01-15 09:20:08 -0500

    Uso de fmt

--- src/main.go
+++ src/main.go
@@ -1,32 +1,43 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
 //Antes tenia otro archivo pero lo perdi, este es uno hecho desde un reto de Platzi
 
 func main() {
 	//Reto: Areas de Rectangulo, Trapecio y Circulo
 	//r
-	br := 20
+	/*br := 20
 	hr := 10
 
 	ar := br * hr
-	fmt.Println("Area del rectangulo: ", ar)
+	fmt.Println("Area del rectangulo: ", ar)*/
 	//t
-	Bt := 20
-	bt := 14
-	ht := 5
+	/*	Bt := 20
+		bt := 14
+		ht := 5
 
-	at := ((bt + Bt) / 2) * ht
-	fmt.Println("Area del trapecio: ", at)
+		at := ((bt + Bt) / 2) * ht
+		fmt.Println("Area del trapecio: ", at)*/
 	//c
-	rc := 10
-	pi := 3.14159265359
-	rc2 := math.Pow(float64(rc), 2)
+	/*	rc := 10
+		pi := 3.14159265359
+		rc2 := math.Pow(float64(rc), 2)
 
-	ac := pi * rc2
-	fmt.Println("Area del circulo: ", ac)
+		ac := pi * rc2
+		fmt.Println("Area del circulo: ", ac)*/
+
+	//Uso de fmt
+	//Printf
+	/*	nombre := "Platzi"
+		num := 10000
+
+		fmt.Printf("%s tiene mas de %d cursos\n", nombre, num)*/
+	//Sprintf
+	/*	message := fmt.Sprintf("%s tiene mas de %d cursos", nombre, num)
+		fmt.Println(message)*/
+	//Tipo de dato
+	/*	n := "asf"
+		m := 123
+		fmt.Printf("n es: %T\n", n)
+		fmt.Printf("m es: %T\n", m)*/
+
 }
